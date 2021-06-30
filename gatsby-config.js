604f841acba8d565706db4b87629ed88c6e70929
James Rosas <jamesadrian777@gmail.com>
2021-06-30 11:00:15 -0500

    primer query usando graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `esta es la description del siteMetada en el archivo gatsby-config.js, que guay!!`,
     author: `@gatsbyjs`,
   },
   plugins: [
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,15 +1,33 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link , graphql} from "gatsby"
 
 import Seo from "../components/seo"
 
-const SecondPage = () => (
+export const query = graphql`
+  query GET_INFO {
+    allSite {
+      edges {
+        node {
+          siteMetadata {
+            description
+          }
+        }
+      }
+    }
+  }
+`
+
+const SecondPage = ({data}) => {
+  const description = data.allSite.edges[0].node.siteMetadata.description;
+  return (
   <>
     <Seo title="Page two" />
-    <h1>Hi from the second page</h1>
+    <h1>Hi from the second page</h1><br/>
+    <p>{description}</p>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </>
 )
+}
 
 export default SecondPage
