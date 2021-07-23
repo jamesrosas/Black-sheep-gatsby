b2d2876f7f83326715583e39bf3110819cf3d5eb
James Rosas <jamesadrian777@gmail.com>
2021-07-22 21:06:09 -0500

    ya quedaron listos los estilos de la product Card, y tambien los flayers de la categoria hombre y mujer en la home

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Link } from 'gatsby'
 import "./styles/ProductCard.css"
 import formatPrice from '../utils/priceFormat'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faArrowCircleRight } from '@fortawesome/free-solid-svg-icons'
+import arrowRight from '../images/right-arrow.png'
 
 const ProductCard = ({products}) => {
     return (
@@ -9,18 +12,24 @@ const ProductCard = ({products}) => {
         {products.map(({node}) =>{
             const price = formatPrice(node.unit_amount)
             return (
-            <div className="card-container" key={node.id}>
-                <div className="img-card_container"> 
-                    <img className="img-card" src={node.product.metadata.img} alt={node.product.name}/>
-                </div>
-                <p className="card-title">{node.product.name}</p>
-                <span>{price}</span>
+            <div className="product-card_container">
                 <Link to={`/${node.id}`}>
-                    <button className="buy-btn">
-                        Comprar
-                    </button>
+                    <div className="card-container" key={node.id}>
+                        <div className="img-card_container"> 
+                            <img className="img-card" src={node.product.metadata.img} alt={node.product.name}/>
+                        </div>
+                        <p className="card-title">{node.product.name}</p>
+                        <span>{price}</span>
+                        <Link to={`/${node.id}`}>
+                            <div className="arrow-container">
+                                <p>Ver Detalles</p>
+                                <img src={arrowRight} width={100}/>
+                            </div>
+                        </Link>
+                    </div>   
                 </Link>
-            </div>   
+                <span className="back-border"></span>
+            </div>
             )
         })}
         </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,9 @@ import { Button } from "./page-2"
 import ProductCard from "../components/ProductCard"
 import Seo from "../components/seo"
 import "./page_styles/Home.css"
+import her from '../images/her.jpg'
+import him from '../images/him.jpg'
+
 
 export const query = graphql`
   query GET_DATA {
@@ -40,8 +43,22 @@ export const query = graphql`
 const IndexPage = ({data}) => (
   <>
     <Seo title={`Los diseños mas inovadores`} description="Bienvenid@ a la tienda con los diseños mas inovadores. Entra ya y descubre todo lo que tenemos para ti"/>
-    <h1>Black Sheep</h1>
-    <p>Descubre todos nuestros diseños</p>
+    <section className="welcome-hero">
+      <span className="welcome-hero_image"></span>
+      <h2 className="welcome-title">Bienvenidx  a Black Sheep</h2>
+    </section>
+    <sectio className="for-they_container">
+      <div className="her-him_flayer">
+        <img src={her} alt="ropa de mujer" width={350} />
+        <h2>FOR HER</h2>
+        <span className="flayer-back_border"></span>
+      </div>
+      <div className="her-him_flayer">
+        <img src={him} alt="ropa de hombre" width={350}/>
+        <h2>FOR HIM</h2>
+        <span className="flayer-back_border"></span>
+      </div>
+    </sectio>
     <section className="products-container">
       <ProductCard products={data.allStripePrice.edges}/>
     </section>
