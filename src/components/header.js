85c5e284917ff07913ffdd34faeaf397289eb37a
James Rosas <jamesadrian777@gmail.com>
2021-07-26 16:47:53 -0500

    carrito en forma de modal prueba 1

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import "./styles/Header.css"
@@ -9,7 +9,13 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 const Header = () => {
   const {cart} = useContext(CartContext)
 
+  const [modal, setModal] = useState(false)
+  
+  const showModal = () => {
+    setModal(!modal)
+  }
   return(
+    <>
     <header className="header-container">
       <div className="icon-container">
         <div className="image-content">
@@ -29,33 +35,40 @@ const Header = () => {
         <nav>
           <ul> 
             <li><Link to="/">Inicio</Link></li>
-            <li><Link to="/cart">
+            <li onClick={showModal}><Link to="/cart">
               <FontAwesomeIcon icon={faShoppingCart} size="xs" />
               {cart.length > 0 ? <span>{cart.length}</span> : null }
             </Link></li>  
           </ul>
         </nav>
       </div>
-      {/* <div className="cart-overlay">
-        <div className="cart-container">
-          <h2>Carrito</h2>
-          {cart.length === 0 && (
-            <p>aun no agregas nada al carrito</p>
-          )}
-          {cart.map( item => {
-            return(
-              <>
-              <img src={item.metadata.img} alt={item.name} />
-              <p>{item.name}</p>
-              </>
-            )
-          }
-            
-          )
-          }
-        </div>
-      </div> */}
     </header>
+    {modal 
+    ?
+    <div onClick={showModal} className="cart-overlay">
+      <div className="cart-modal">
+        <button onClick={showModal}>X</button>
+        <h2>Carrito</h2>
+        {cart.length === 0 && (
+          <p>aun no agregas nada al carrito</p>
+        )}
+        {cart.map( item => {
+          return(
+          <>
+            <img src={item.metadata.img} alt={item.name} width={150} />
+            <p>{item.name}</p>
+          </>
+          )
+        }      
+        )
+        }
+      </div>
+    </div>
+    :
+    null
+    }
+   
+    </>
   )
 }
 
