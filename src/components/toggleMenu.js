3fe0e8291950bc9bd3a02fe4e715b336a005a574
James Rosas <jamesadrian777@gmail.com>
2021-08-10 17:39:25 -0500

    mejore la ux del modal de menu en modo mobile en su li de categorys para mostrar un pequeño modal de su submenu atraves de un click

--- src/components/toggleMenu.js
+++ src/components/toggleMenu.js
@@ -1,15 +1,23 @@
 import { Link } from 'gatsby'
-import React from 'react'
+import React, { useState } from 'react'
 import "./styles/toggleMenu.css"
 
 const ToggleMenu = ({onClick}) => {
+
+    const [subMenu, setSubMenu] = useState(false)
+    const showSubMenu = () => {
+        setSubMenu(!subMenu)
+    }
+    
     return (
         <div className="toggle-menu_container">
             <ul>
                 <li id="toggle-home"><Link onClick={onClick} to="/">Home</Link></li>
-                <li>
-                    <details>
-                        <summary>Categorys</summary>
+                <li onClick={showSubMenu} id="toggle-categorys">
+                    Categorys
+                    {subMenu && (
+                    <div id="toggle-categorys_submenu">
+                        <span onClick={showSubMenu}>x</span>
                         <ul>
                             <li className="li-details">
                                 <Link onClick={onClick} to="/ropa-mujer">For her</Link>
@@ -18,7 +26,8 @@ const ToggleMenu = ({onClick}) => {
                                 <Link onClick={onClick} to="/ropa-hombre">For him</Link>
                             </li>
                         </ul>
-                    </details>
+                    </div>
+                    )}
                 </li>
             </ul>
         </div>
